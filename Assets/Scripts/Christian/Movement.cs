6626b19078c9a6ea0fd520b0e051046914c9b909
chrizmoko <christianmok2000@yahoo.com>
2019-11-04 10:28:11 -0800

    Obstacle prefab -Christian

---
+++ Assets/Scripts/Christian/Movement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movement
+{
+    public float xScroll; // Scroll speed of level
+    protected Vector2 velocity;
+    protected float xAccel = 0, yAccel = 0;
+
+    public Movement(float xScroll)
+    {
+        this.xScroll = xScroll;
+        velocity = new Vector2(0, 0);
+    }
+
+    public Movement(float xScroll, Vector2 velocity)
+    {
+        this.xScroll = xScroll;
+        this.velocity = velocity;
+    }
+
+    public abstract Vector2 Execute();
+
+    public Vector2 TrueVelocity()
+    {
+        return new Vector2(xScroll + velocity.x, velocity.y);
+    }
+
+    public Vector2 Velocity()
+    {
+        return velocity;
+    }
+}
+
+public class Static : Movement
+{
+    public Static(float xScroll) : base(xScroll)
+    {
+
+    }
+
+    public override Vector2 Execute()
+    {
+        return new Vector2(xScroll, 0);
+    }
+}
+
+// Don't use, does not work properly
+public class ZigZag : Movement
+{
+    public ZigZag(float xScroll, float amplitude, float yVel) : base(xScroll)
+    {
+
+    }
+
+    public ZigZag(float xScroll, Vector2 velocity, float amplitude, float yVel) : base(xScroll, velocity)
+    {
+
+    }
+
+    public override Vector2 Execute()
+    {
+        return new Vector2();
+    }
+}
+
+// Don't use, does not work properly
+public class SineWave : Movement
+{
+    protected float amplitude;
+    protected float period;
+    private float time = 0;
+
+    public SineWave(float xScroll, float amplitude) : base (xScroll)
+    {
+        this.amplitude = amplitude;
+        velocity = Calculate();
+    }
+
+    public override Vector2 Execute()
+    {
+        return Calculate();
+    }
+
+    private Vector2 Calculate()
+    {
+        time += 0.01f;
+        return new Vector2(xScroll + velocity.x, xScroll * amplitude * Mathf.Cos(xScroll * time) + velocity.y);
+    }
+}
---
+++ Assets/Scripts/Christian/ObstaclePrefab.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePrefab : MonoBehaviour
+{
+    public float xScroll;
+    public int health = -1, damage = 1;
+    public Movement movement; // Defines the path the obstacle will take
+
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+    private float xSize; // Half the size of the obstacle for offscreen object destruction purposes
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setup Obstacle Movement
+        movement = new Static(xScroll);
+        Vector2 velocity = movement.TrueVelocity();
+ 
+        // Set movement of the game object (the obstacle)
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = velocity;
+
+        // Get boundaries for off camera game object despawning
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        // Edge offset of object
+        xSize = transform.localScale.x / 2;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Obstacle movement
+        rb.velocity = movement.Execute();
+
+        // Debug.Log("w: " + screenBounds.x + " h: " + screenBounds.y);
+        Debug.Log("x: " + transform.position.x + " y: " + transform.position.y);
+
+        // Object gets uninstantiated once off the camera (dependent of x axis position)
+        if (xScroll > 0 && transform.position.x > -screenBounds.x + xSize)
+        {
+            Debug.Log("right");
+            Destroy(this.gameObject);
+        }
+        if (xScroll < 0 && transform.position.x < screenBounds.x - xSize)
+        {
+            Debug.Log("left");
+            Destroy(this.gameObject);
+        }
+    }
+}
