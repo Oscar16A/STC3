62613e4a6af3a4f91deee1eaddc83dc5b8525748
Lillian Won <lwon1@uci.edu>
2019-11-22 09:47:47 -0800

    Changed HighScore design

--- Assets/Scripts/Lillian/HighscoreScript.cs
+++ Assets/Scripts/Lillian/HighscoreScript.cs
@@ -6,93 +6,14 @@ using UnityEngine.UI;
 public class HighscoreScript : MonoBehaviour
 {
     // Start is called before the first frame update
-	private Transform entryContainer;
-	private Transform entryTemplate;
-	private List<HighscoreEntry> highscoreEntryList;
-	private List<Transform> highscoreEntryTransformList;
 
-	void Awake()
+	public Text score; 
+
+	public void testingFunct()
 	{
-		entryContainer = transform.Find("highscoreEntryContainer");
-		entryTemplate = entryContainer.Find("highscoreEntryTemplate");
-
-		entryTemplate.gameObject.SetActive(false); // hide the def template for dupes
-
-		highscoreEntryList = new List<HighscoreEntry>()
-		{
-			new HighscoreEntry{ score = 521854, name = "AAA" }
-		};
-
-		highscoreEntryTransformList = new List<Transform>(); 
-
-		foreach ( HighscoreEntry highscoreEntry in highscoreEntryList )
-		{
-			CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
-		}
+		int number = Random.Range(1, 7);
+		score.text = number.ToString();
+	} 
 
 
-	}
-
-	private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry,
-				Transform container, List<Transform> transformList)
-	{
-			float templateHeight = 20f;
-			Transform entryTransform = Instantiate(entryTemplate, container);
-			RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-
-			entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
-
-			entryTransform.gameObject.SetActive(true);
-
-			int rank = transformList.Count + 1; 
-			string rankString;
-			switch (rank) {
-			case 1: 
-				rankString = "1ST";
-				break;
-
-			case 2: 
-				rankString = "2ND";
-				break;
-
-			case 3:
-				rankString = "4TH";
-				break;
-
-			default:
-				rankString = rank + "TH"; 
-				break;
-			}
-
-			entryTransform.Find("posText").GetComponent<Text>().text = rankString;
-
-
-			// temp score
-			int score = highscoreEntry.score;
-
-			entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
-
-			string name = highscoreEntry.name;
-			entryTransform.Find("nameText").GetComponent<Text>().text = name;
-
-			transformList.Add(entryTransform);
-	}
-
-	// high score entry
-	private class HighscoreEntry
-	{
-		public int score;
-		public string name;
-	}
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
