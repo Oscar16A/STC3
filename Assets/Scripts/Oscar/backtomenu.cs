199d00e1842c86a68d4a6080163829cef5b4a47a
Oscar Amarillas <oscar16amarillas@gmail.com>
2019-12-02 14:00:57 -0800

    presentation build

---
+++ Assets/Scripts/Oscar/backtomenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backtomenu : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Test Main Menu");
+        }
+    }
+}
---
+++ Assets/Scripts/Oscar/testmenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmenu : MonoBehaviour
+{
+    public void loadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Test Gameplay");
+    }
+
+    public void quit()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
